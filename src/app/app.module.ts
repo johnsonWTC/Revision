9475fb1710288671418f5c7b942ee233579cb622
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-06 15:35:27 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HideComponent } from './hide/hide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HideComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hide/hide.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hide',
+  templateUrl: './hide.component.html',
+  styleUrls: ['./hide.component.css']
+})
+export class HideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  divValue = "hi from the class"
+
+}
